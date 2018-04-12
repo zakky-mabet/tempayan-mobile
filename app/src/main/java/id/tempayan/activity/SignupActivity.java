7b11974491007ebd2046e5279100b4abb99378f5
Muhamad Zakky <muhamadzakky45@gmail.com>
2018-04-12 08:57:17 +0700

    ke laptop

--- app/src/main/java/id/tempayan/activity/SignupActivity.java
+++ app/src/main/java/id/tempayan/activity/SignupActivity.java
@@ -1,29 +1,61 @@
 package id.tempayan.activity;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import id.tempayan.R;
+import id.tempayan.apihelper.BaseApiService;
+import id.tempayan.apihelper.UtilsApi;
 
 public class SignupActivity extends AppCompatActivity {
 
+    public static final int MIN_PASSWORD_LENGTH = 8;
 
+    @BindView(R.id.etNik)
+    TextView etNik;
+    @BindView(R.id.etNamaLengkap)
+    TextView etNamaLengkap;
+    @BindView(R.id.etEmail)
+    TextView etEmail;
+    @BindView(R.id.etSandi)
+    TextView etSandi;
+    @BindView(R.id.etKonfirmasiSandi)
+    TextView etKonfirmasiSandi;
+    @BindView(R.id.etNomorHp)
+    EditText etNomorHp;
+//    @BindView(R.id.etTanggalLahir)
+//    EditText etTanggalLahir;
+//    @BindView(R.id.rgJenisKelamin)
+//    RadioGroup rgJenisKelamin;
+//    @BindView(R.id.rbLakiLaki)
+//    RadioButton rbLakiLaki;
+//    @BindView(R.id.rbPerempuan)
+//    RadioButton rbPerempuan;
     @BindView(R.id.tvSignin)
     TextView tvSignin;
-    DatePickerDialog datePickerDialog;
+    @BindView(R.id.bSignup)
+    Button bSignup;
 
-    @BindView(R.id.etTanggalLahir)
-    EditText etTanggalLahir;
+    DatePickerDialog datePickerDialog;
+    ProgressDialog loading;
+    Context mContext;
+    BaseApiService mApiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,24 +73,97 @@ public class SignupActivity extends AppCompatActivity {
             }
         }); // end tvSignin setOnClickListener
 
-        etTanggalLahir.setOnClickListener(new View.OnClickListener() {
+//        etTanggalLahir.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // calender class's instance and get current date , month and year from calender
+//                final Calendar c = Calendar.getInstance();
+//                int mYear = c.get(Calendar.YEAR); // current year
+//                int mMonth = c.get(Calendar.MONTH); // current month
+//                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+//                // date picker dialog
+//                datePickerDialog = new DatePickerDialog(SignupActivity.this, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//                            etTanggalLahir.setText(year+"-"+monthOfYear+"-"+dayOfMonth);
+//                            }
+//                        }, mYear, mMonth, mDay);
+//                datePickerDialog.show();
+//            }
+//        });
+
+        mContext = this;
+        mApiService = UtilsApi.getAPIService();
+
+        initComponents();
+
+    }
+
+    private void initComponents() {
+        bSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // calender class's instance and get current date , month and year from calender
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR); // current year
-                int mMonth = c.get(Calendar.MONTH); // current month
-                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-                // date picker dialog
-                datePickerDialog = new DatePickerDialog(SignupActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                            etTanggalLahir.setText(year+"-"+monthOfYear+"-"+dayOfMonth);
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
+                if (etNik.getText().toString().equals("")) {
+                    etNik.setError("NIK diperlukan");
+                    etNik.requestFocus();
+                    return;
+                } else if (etNamaLengkap.getText().toString().equals("")) {
+                    etNamaLengkap.setError("Nama Lengkap diperlukan");
+                    etNamaLengkap.requestFocus();
+                    return;
+                } else if (etEmail.getText().toString().equals("")) {
+                    etEmail.setError("Email diperlukan");
+                    etEmail.requestFocus();
+                    return;
+                } else if (!isValidEmail(etEmail.getText().toString())) {
+                    etEmail.setError("Format email salah");
+                    etEmail.requestFocus();
+                    return;
+                } else if (etSandi.getText().toString().equals("")) {
+                    etSandi.setError("Sandi diperlukan");
+                    etSandi.requestFocus();
+                    return;
+                } else if (etSandi.length() < MIN_PASSWORD_LENGTH) {
+                    etSandi.setError("Sandi Minimal 8 Karakter");
+                    etSandi.requestFocus();
+                    return;
+                } else if (etKonfirmasiSandi.getText().toString().equals("")) {
+                    etKonfirmasiSandi.setError("Konfirmasi Sandi diperlukan");
+                    etKonfirmasiSandi.requestFocus();
+                    return;
+                } else if (!etKonfirmasiSandi.getText().toString().matches(etSandi.getText().toString()) ) {
+                    etKonfirmasiSandi.setError("Konfirmasi Sandi tidak Valid");
+                    etKonfirmasiSandi.requestFocus();
+                    return;
+                }  else if (etNomorHp.getText().toString().equals("")) {
+                    etNomorHp.setError("Nomor Handphone diperlukan");
+                    etNomorHp.requestFocus();
+                    return;
+                } else {
+                    loading = ProgressDialog.show(mContext, null, etNamaLengkap.getText()+" Harap Tunggu...", true, false);
+                    requestRegister();
+                }
             }
         });
+    }
 
+    private void requestRegister() {
+// test
+    }
+
+    public static boolean isValidEmail(String email) {
+        boolean validate;
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        String emailPattern2 = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+\\.+[a-z]+";
+
+        if (email.matches(emailPattern)) {
+            validate = true;
+        } else if (email.matches(emailPattern2)) {
+            validate = true;
+        } else {
+            validate = false;
+        }
+
+        return validate;
     }
 }
